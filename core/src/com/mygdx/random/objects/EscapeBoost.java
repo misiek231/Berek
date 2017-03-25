53d1405d72c095c830a4d56820b93db79e2ce3e2
DESKTOP-C1IV3B2\MohairApps <mohairapp@hotmail.com>
2017-03-25 09:41:50 +0100

    escape

---
+++ core/src/com/mygdx/random/objects/EscapeBoost.java
@@ -0,0 +1,19 @@
+package com.mygdx.random.objects;
+
+import com.mygdx.game.Berek;
+import com.mygdx.players.Player;
+
+public class EscapeBoost extends RandomObject {
+
+	public EscapeBoost(Berek game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addEffectsToPlayers(Player playerTouchet, Player playerNotTouchet) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
