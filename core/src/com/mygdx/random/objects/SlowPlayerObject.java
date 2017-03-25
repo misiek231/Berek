180c813a5a0ee501c7d33708ceb88741a63f7573
DESKTOP-C1IV3B2\MohairApps <mohairapp@hotmail.com>
2017-03-25 08:35:06 +0100

    init

---
+++ core/src/com/mygdx/random/objects/SlowPlayerObject.java
@@ -0,0 +1,19 @@
+package com.mygdx.random.objects;
+
+import com.mygdx.game.Berek;
+import com.mygdx.players.Player;
+
+public class SlowPlayerObject extends RandomObject {
+
+	public SlowPlayerObject(Berek game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addEffectsToPlayers(Player player) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
